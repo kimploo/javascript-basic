35d02edb38e875a818af1c3477352783e2798b55
Hong Shik Branden Kim <kimploo@gmail.com>
2024-08-16 18:59:47 +0900

    commit

--- kanto-pokemon/index.js
+++ kanto-pokemon/index.js
@@ -1,5 +1,3 @@
-console.log("You have connected...");
-
 const api = {
   getPokemons() {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
@@ -12,88 +10,92 @@ const api = {
           fetch(url)
             .then((response) => response.json())
             .then(function (pokeData) {
-              renderPokemon(pokeData);
+              render.one(pokeData);
             });
+        });
+      });
+  },
+
+  getPokemonsSorted() {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((response) => response.json())
+      .then(function (allpokemon) {
+        Promise.all(
+          allpokemon.results.map((pokemon) => {
+            return fetch(pokemon.url).then((r) => r.json());
+          }),
+        ).then((pokemons) => {
+          pokemons.forEach((pokemon) => {
+            render.one(pokemon);
+          });
         });
       });
   },
 };
 
-function renderEverything() {
-  let allPokemonContainer = document.querySelector("#poke-container");
-  allPokemonContainer.innerText = "";
-  fetchKantoPokemonSorted();
+const render = {
+  one(pokeData) {
+    let allPokemonContainer = document.getElementById("poke-container");
+    let pokeContainer = document.createElement("div"); //div will be used to hold the data/details for indiviual pokemon.{}
+    pokeContainer.classList.add("ui", "card");
 
-  document.querySelector("#delete-btn").style.display = "block";
-}
+    ui.drawPokeImage(pokeData, pokeContainer);
 
-function fetchKantoPokemonSorted() {
-  fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-    .then((response) => response.json())
-    .then(function (allpokemon) {
-      Promise.all(
-        allpokemon.results.map((pokemon) => {
-          return fetch(pokemon.url).then((r) => r.json());
-        }),
-      ).then((pokemons) => {
-        pokemons.forEach((pokemon) => {
-          renderPokemon(pokemon);
-        });
-      });
+    let pokeName = document.createElement("h4");
+    pokeName.innerText = pokeData.name;
+
+    let pokeNumber = document.createElement("p");
+    pokeNumber.innerText = `#${pokeData.id}`;
+
+    let pokeTypes = document.createElement("ul"); //ul list will hold the pokemon types
+
+    ui.drawType(pokeData.types, pokeTypes); // helper function to go through the types array and create li tags for each one
+
+    pokeContainer.append(pokeName, pokeNumber, pokeTypes); //appending all details to the pokeContainer div
+    allPokemonContainer.appendChild(pokeContainer); //appending that pokeContainer div to the main div which will                                                             hold all the pokemon cards
+  },
+
+  allSorted() {
+    let allPokemonContainer = document.querySelector("#poke-container");
+    allPokemonContainer.innerText = "";
+    api.getPokemonsSorted();
+
+    document.querySelector("#delete-btn").style.display = "block";
+  },
+};
+
+const ui = {
+  drawType(types, ul) {
+    types.forEach(function (type) {
+      let typeLi = document.createElement("li");
+      typeLi.innerText = type["type"]["name"];
+      ul.append(typeLi);
     });
-}
+  },
 
-function renderPokemon(pokeData) {
-  let allPokemonContainer = document.getElementById("poke-container");
-  let pokeContainer = document.createElement("div"); //div will be used to hold the data/details for indiviual pokemon.{}
-  pokeContainer.classList.add("ui", "card");
+  drawPokeImage(pokeData, containerDiv) {
+    let pokeImgContainer = document.createElement("div");
+    pokeImgContainer.classList.add("image");
 
-  createPokeImage(pokeData, pokeContainer);
+    let pokeImage = document.createElement("img");
+    pokeImage.srcset = pokeData.sprites.front_default;
 
-  let pokeName = document.createElement("h4");
-  pokeName.innerText = pokeData.name;
+    pokeImgContainer.append(pokeImage);
+    containerDiv.append(pokeImgContainer);
+  },
 
-  let pokeNumber = document.createElement("p");
-  pokeNumber.innerText = `#${pokeData.id}`;
-
-  let pokeTypes = document.createElement("ul"); //ul list will hold the pokemon types
-
-  createTypes(pokeData.types, pokeTypes); // helper function to go through the types array and create li tags for each one
-
-  pokeContainer.append(pokeName, pokeNumber, pokeTypes); //appending all details to the pokeContainer div
-  allPokemonContainer.appendChild(pokeContainer); //appending that pokeContainer div to the main div which will                                                             hold all the pokemon cards
-}
-
-function createTypes(types, ul) {
-  types.forEach(function (type) {
-    let typeLi = document.createElement("li");
-    typeLi.innerText = type["type"]["name"];
-    ul.append(typeLi);
-  });
-}
-
-function createPokeImage(pokeData, containerDiv) {
-  let pokeImgContainer = document.createElement("div");
-  pokeImgContainer.classList.add("image");
-
-  let pokeImage = document.createElement("img");
-  pokeImage.srcset = pokeData.sprites.front_default;
-
-  pokeImgContainer.append(pokeImage);
-  containerDiv.append(pokeImgContainer);
-}
-
-function deleteEverything(event) {
-  event.target.style = "none";
-  let allPokemonContainer = document.querySelector("#poke-container");
-  allPokemonContainer.innerText = "";
-}
+  removeAll(event) {
+    event.target.style = "none";
+    let allPokemonContainer = document.querySelector("#poke-container");
+    allPokemonContainer.innerText = "";
+    document.querySelector("#delete-btn").style.display = "none";
+  },
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   let generateBtn = document.querySelector("#generate-pokemon");
-  generateBtn.addEventListener("click", renderEverything);
-  document
-    .querySelector("#delete-btn")
-    .addEventListener("click", deleteEverything);
+  generateBtn.addEventListener("click", render.allSorted);
+  document.querySelector("#delete-btn").addEventListener("click", ui.removeAll);
+  document.querySelector("#delete-btn").style.display = "block";
   api.getPokemons();
 });
